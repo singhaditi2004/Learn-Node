2840bc0e8fb8a5775e87b59a7a690b74bf89dc10
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-11 22:22:39 +0530

    Added

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+import dotenv from 'dotenv';
 import http from 'http';
+
+dotenv.config(); // Load environment variables from .env file
+
 const PORT = process.env.PORT || 3000; // default to 3000 if PORT is not set
 
 const server = http.createServer((req, res) => {
+    console.log(req.url);
+    console.log(res.method);
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Hello world</h1>');
 });
