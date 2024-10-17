d554a5cc6476e956f7b1a832596c598f808d912f
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-17 13:39:49 +0530

    added users route

--- server2.js
+++ server2.js
@@ -32,30 +32,43 @@ const getUserByIdHandler = (req, res) => {
   }
   res.end();
 };
+const notFoundHandler = (req, res) => {
+  res.statusCode = 404;
+  res.write(JSON.stringify({ message: "Route not found" }));
+  res.end();
+};
+
+const createUserHandler = (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    const user = JSON.parse(body);
+    users.push(user);
+    res.statusCode = 201;
+    res.write(JSON.stringify(user));
+    res.end();
+  });
+};
 const server = createServer((req, res) => {
   logger(req, res, () => {
-    if (req.url === "/api/users" && req.method === "GET") {
-      res.setHeader("Content-Type", "application/json");
-      res.write(JSON.stringify(users));
-      res.end();
-    } else if (req.url.match(/\/api\/users\/[0-9]+/) && req.method === "GET") {
-      const id = req.url.split("/")[3];
-      const user = users.find((user) => user.id == parseInt(id));
-      res.setHeader("Content-Type", "application/json");
-      if (user) {
-        res.write(JSON.stringify(user));
-      } else {
-        res.statusCode = 404;
-        res.write(JSON.stringify({ message: "User not found" }));
+    jsonMiddleware(req, res, () => {
+      if (req.url === "/api/users" && req.method === "GET") {
+        getUsersHandler(req, res);
+      } else if (
+        req.url.match(/\/api\/users\/[0-9]+/) &&
+        req.method === "GET"
+      ) {
+        getUserByIdHandler(req, res);
+      } 
+       else if(req.url==='/api/users' && req.method=="POST"){
+        createUserHandler(req, res);
+       }
+      else {
+        notFoundHandler(req, res);
       }
-      res.end();
-      console.log(id);
-    } else {
-      res.setHeader("Content-Type", "application/json");
-      res.statusCode = 404;
-      res.write(JSON.stringify({ message: "Route not found" }));
-      res.end();
-    }
+    });
   });
 });
 
