230c83d6d7f1473db76566b17182a18c0f28d75f
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-18 15:55:12 +0530

    added file

---
+++ fsdemo.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+/*fs.readFile("./test.txt", "utf8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+});
+
+const data = fs.readFileSync("./test.txt", "utf8");
+console.log(data);*/
+
+
+fs.readFile("./test.txt", "utf8")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
