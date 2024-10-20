e96b8a2f11be4bb3fa46fd2646af3e24cd28b510
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-20 23:30:08 +0530

    App.js changed

--- Scaler/app.js
+++ Scaler/app.js
@@ -1,4 +1,18 @@
 const express = require("express");
 
 const app = express();
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/contact", (res, req) => {
+  res.send("Contact page");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
