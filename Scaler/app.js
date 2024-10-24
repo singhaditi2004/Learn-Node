a7f369d7404b85c249de0b5d6419ffd3a61f85a0
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-24 19:32:01 +0530

    Courses

--- Scaler/app.js
+++ Scaler/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
 const app = express();
+const courses = [
+  { id: 1, name: "JS" },
+  { id: 2, name: "Java" },
+  { id: 3, name: "Python" },
+  { id: 4, name: "C++" },
+  { id: 5, name: "C#" },
+];
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -11,6 +18,14 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (res, req) => {
   res.send("Contact page");
+});
+
+app.get("/courses/:id", (req, res) => {
+  let course = courses.find((cour) => cour.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("Course Not found");
+  }
+  res.send(course);
 });
 
 app.listen(3000, () => {
