faba099b06dadbe6b80d94b638335176226ca1c0
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-30 14:44:42 +0530

    added mprgan

--- Scaler/app.js
+++ Scaler/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json);
+app.use(morgan('tiny'));
 const courses = [
   { id: 1, name: "JS" },
   { id: 2, name: "Java" },
